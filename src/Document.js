c244fa4261d0a0edbaff9e53feaea1e24c5e56a7
Martin <mdsugarev@gmail.com>
2022-12-08 14:24:25 +0200

    added listeners

---
+++ src/Document.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react'
+import './Document.css'
+
+
+export const Document = ({title, content}) => {
+
+    const [active, setActive] = useState(true)
+    const [btnFromTop, setBtnFromTop] = useState(null)
+    const [btnPosition,setBtnPosition] = useState(null) 
+
+    useEffect(() => {
+        document.querySelector('.container').addEventListener('scroll', () => {
+      const length = document.querySelector('.container').scrollTop  
+     const btn =  document.querySelector('button').offsetTop
+     setBtnFromTop(length)
+     setBtnPosition(btn)
+      })
+    }, [])
+
+    useEffect(() => {
+  const a = document.querySelector('.container').scrollTop + document.querySelector('.container').clientHeight
+   
+  if(a >= btnPosition){
+      setActive(() => false)
+  }
+    
+    }, [btnFromTop])
+
+
+    return (
+        <div className="container">
+            <h1 className="title">{title}</h1>
+            <p className="content">{content}</p>
+            <button disabled={active ? true : false} >I Agree</button>
+        </div>
+    )
+}
