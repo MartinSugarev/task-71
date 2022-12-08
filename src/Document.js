30973957039517125ae3535daf6d5bb0694521a0
Martin <mdsugarev@gmail.com>
2022-12-08 14:27:29 +0200

    ddd

--- src/Document.js
+++ src/Document.js
@@ -8,14 +8,22 @@ export const Document = ({title, content}) => {
     const [btnFromTop, setBtnFromTop] = useState(null)
     const [btnPosition,setBtnPosition] = useState(null) 
 
-    useEffect(() => {
-        document.querySelector('.container').addEventListener('scroll', () => {
-      const length = document.querySelector('.container').scrollTop  
-     const btn =  document.querySelector('button').offsetTop
-     setBtnFromTop(length)
-     setBtnPosition(btn)
-      })
-    }, [])
+    // useEffect(() => {
+    //     document.querySelector('.container').addEventListener('scroll', () => {
+    //   const length = document.querySelector('.container').scrollTop  
+    //  const btn =  document.querySelector('button').offsetTop
+    //  setBtnFromTop(length)
+    //  setBtnPosition(btn)
+    //   })
+    // }, [])
+
+
+ const scroll = () => {
+    const length = document.querySelector('.container').scrollTop  
+    const btn =  document.querySelector('button').offsetTop
+    setBtnFromTop(length)
+    setBtnPosition(btn)
+ }
 
     useEffect(() => {
   const a = document.querySelector('.container').scrollTop + document.querySelector('.container').clientHeight
@@ -28,7 +36,7 @@ export const Document = ({title, content}) => {
 
 
     return (
-        <div className="container">
+        <div onScroll={() => scroll()} className="container">
             <h1 className="title">{title}</h1>
             <p className="content">{content}</p>
             <button disabled={active ? true : false} >I Agree</button>
